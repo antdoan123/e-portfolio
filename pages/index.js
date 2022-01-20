a2b6d93a7087fd4c2bd75992ca4504ace0dfd59d
antdoan123 <aqdoan@cpp.edu>
2022-01-20 02:15:32 -0800

    Fixed

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
                     </div>
             </div>
                 <div className="py-32 continer flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-ful">
-                    <Image src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortRound&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Happy&eyebrowType=Default&mouthType=Default&skinColor=Light'/>          
+                    <img src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortRound&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Happy&eyebrowType=Default&mouthType=Default&skinColor=Light'/>          
                 </div>
             
         </header>
