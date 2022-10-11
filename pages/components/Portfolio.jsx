7459d98fc7f79e4df7306e5006987787a2ed58e9
antdoan123 <aqdoan@cpp.edu>
2022-10-11 16:54:24 -0700

    i give up

--- pages/components/Portfolio.jsx
+++ pages/components/Portfolio.jsx
@@ -47,8 +47,8 @@ function Portfolio() {
               <div className="mt-6 grid grid-cols md:grid-cols-2 lg:grid-cols-3 gap-8" >
                   {portfolios.map(({id, tech, title, description, href}) => (
 
-                      <div key={id} className="border px-8 py-8 rounded-3xl outline outline-1 outline-blue-300">
-                        
+                      <div className="border px-8 py-8 rounded-3xl outline outline-1 outline-blue-300">
+                        <li key={id}/>
                         <h2 className="text-base font-medium text-blue-300 mb-1">{tech}</h2>
                             <h1 className="text-2xl font-semibold mb-3">{title}</h1>
                             <p className="leading-relaxed mb-3">{description} 
