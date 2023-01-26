9407259e429c4dfc429abfadae69a47cef2e017b
antdoan123 <aqdoan@cpp.edu>
2023-01-26 04:42:41 -0800

    fix index

--- pages/_app.js
+++ pages/_app.js
@@ -9,25 +9,27 @@ import Head from 'next/head';
 
 function MyApp() {
   return (
-    <html>
+    <main>
     <Head>
       <title>{"Anthony Doan Website"}</title>
+      <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
       <meta name="description" content="Discover the portfolio of Anthony Doan, a highly skilled software developer. View his past projects and get in touch to bring your next project to life." />
       <meta
           name="keywords"
           content="Anthony Doan, Anthony, Doan, antdoan, github, developer, cpp, software"
         />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       <link rel='icon' href='./logo.png'></link>
     </Head>
-      <body>
+      <div>
         <NavBar/>
         <Home />
         <Portfolio/>
         <Skills/>
         <Contact/>
         <Footer/>
-      </body>
-    </html>
+      </div>
+    </main>
   );
   
 }
